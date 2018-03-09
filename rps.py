f01d2eda6dd439fd3654c469fd0a3b9002783c29
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-03-09 09:33:45 -0600

    FInish database integration
    Database is integrated, but only stores in memory for now. To use real database, either use locally stored database file, or port to a centralized SQL service.

--- rps.py
+++ rps.py
@@ -5,13 +5,20 @@ from translate import str_to_num
 from proc import calc_freq, arb
 
 move_list = [0, 1, 2]
-connection = sql.connect(":memory:")
+connection = sql.connect(":memory:") # HACK: Use a database file to keep permanent changes
 db = connection.cursor()
+db.execute("DROP TABLE IF EXISTS rps") # DEBUG
 db.execute("CREATE TABLE rps (move)")
+for val in move_list:
+    db.execute("INSERT INTO rps VALUES (?)", (val,))
+    connection.commit()
+# HACK: To get all, use SQL select, then db.fetch___
 
 def add_move(move):
     move_list.append(int(move))
     db.execute("INSERT INTO rps VALUES (?)", (move,))
+    connection.commit()
+    db.execute("SELECT * FROM rps")
     print(db.fetchall())
 
 def spin(percent):
@@ -26,14 +33,17 @@ def spin(percent):
     else:
         raise Exception()
 
-move = input("Enter move(rock,paper,or scissors): ")
+print("Enter a move for the prompt. Enter 'stop' to stop.")
+while(42):
+    move = input("Enter move(rock,paper,or scissors) :: ")
+    if(move == "stop"):
+        print("Ending program...")
+        break
 
-move = str_to_num(move)
+    move = str_to_num(move)
 
-add_move(move)
-connection.commit()
+    add_move(move)
 
-choice = spin(calc_freq(move_list))
-result = arb(move,choice)
-db.fetchall() # for debugging, remove before commit
+    choice = spin(calc_freq(move_list))
+    result = arb(move,choice)
 connection.close()
