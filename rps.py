86bec0a9616bc979a1024d0abf32dd8da94b862f
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-03-08 23:41:39 -0600

    Frame database integration
    Using the SQLite relational database
    Also added custom database ignore

--- rps.py
+++ rps.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
+import sqlite3 as sql
 from random import randint
 from translate import str_to_num
 from proc import calc_freq, arb
 
 move_list = [0, 1, 2]
+connection = sql.connect(":memory:")
+db = connection.cursor()
+db.execute("CREATE TABLE rps (move)")
 
 def add_move(move):
     move_list.append(int(move))
+    db.execute("INSERT INTO rps VALUES (?)", (move,))
+    print(db.fetchall())
 
 def spin(percent):
     rand = randint(1,100)
@@ -25,6 +31,9 @@ move = input("Enter move(rock,paper,or scissors): ")
 move = str_to_num(move)
 
 add_move(move)
+connection.commit()
 
 choice = spin(calc_freq(move_list))
 result = arb(move,choice)
+db.fetchall() # for debugging, remove before commit
+connection.close()
