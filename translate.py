0b85279f9f0e7d6c22471c64f8ae0f00c91d1751
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-03-08 18:45:54 -0600

    Fix function names

--- translate.py
+++ translate.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-def translate(num):
     if(num == 0):
+def num_to_str(num):
         return "rock"
     elif (num == 1):
         return "paper"
     elif (num == 2):
         return "scissors"
     else:
-        raise ValueError("The value entered was not between 0 and 2")
+        raise ValueError("The value entered was not between 0 and 2")def str_to_num(str):
